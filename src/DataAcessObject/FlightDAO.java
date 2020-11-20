2680b43b6981435adcd6278649b3ec77da07eb6f
dan-555 <dan.belissa@edu.ece.fr>
2020-11-20 15:56:40 +0100

    Revert "bug error"
    This reverts commit 2637e37724ebadbe6f515557128b32846be7925c.

--- src/DataAcessObject/FlightDAO.java
+++ src/DataAcessObject/FlightDAO.java
@@ -1,7 +1,14 @@
 package DataAcessObject;
+<<<<<<< Updated upstream
 
+public class FlightDAO {
+    
+=======
 import model.*;
 
 public class FlightDAO {
-
+    
+    
+    
+>>>>>>> Stashed changes
 }
