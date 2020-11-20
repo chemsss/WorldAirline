3687bd0019772934b5d4df1e7059091d7cb3c2fc
chemsss <61418782+chemsss@users.noreply.github.com>
2020-11-20 16:33:38 +0100

    Revert "Merge branch 'develop-d' of https://github.com/dan-555/worldAirline into develop-d"
    This reverts commit 1db5d4f3d0c7f43721b4a10154afb63ddd1974a5, reversing
    changes made to 5099448eb3d2a775bdfe389ac2eb3c6555fe6808.

---
+++ src/model/Booking.java
@@ -0,0 +1,12 @@
+package model;
+import java.util.ArrayList;
+import java.sql.*;
+
+public class Booking {
+    
+    private int bookingNo;//booking no
+    private Date bookingDate;// booking date
+    private ArrayList<Ticket> tickets; //tickets
+   // private CustomerAccount customerAccount; //customer account
+    
+}
